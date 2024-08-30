befb2e17153303cd7dc946398a7552c20a392d15
britokaiquee <kaique.brito.rob@gmail.com>
2024-08-30 16:14:38 -0300

    Comandos aprimorados e comando de reiniciar calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,60 +1,58 @@
-import sys
 import os
 
 
-############################ FUNÇÃO PRINCIPAL ##################################
+# Função para limpar a tela, compatível com Windows e Unix-based OS
+def limpar_tela():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
+# Função principal
 def main():
-    # Versão em que compilei pela primeira vez
-    print('\nCalculadora v0.15.0 - by: Kaique.\n')
-    print('"l" para listar operadores e comandos.\n\n')
-
-    while True:
-        # Solicita o primeiro número ou comando
-        numero = obter_entrada('Primeiro número:\n')
-        print()
+    try:
+        print('Calculadora v0.16.0 - by: Kaique.\n')
+        print('"l" para listar operadores e comandos.\n\n')
 
         while True:
-            operador = input('Operador:\n').lower()
-
-            if processar_comando(operador):
-                continue
-
-            if operador == 'f':
-                limpar_tela()
-                print('"l" para listar operadores e comandos.\n')
-                break
-
-            operadores = ['+', '-', '*', '**', '/', '//', '%', '%%', '&']
-
-            # Verifica se o operador é válido antes de continuar
-            if operador not in operadores:
-                limpar_tela()
-                print('Operador ou comando inválido. Tente novamente.')
-                print()
-                continue
-
-            # Solicita o próximo número ou comando
+            # Solicita o primeiro número ou comando
+            numero = obter_entrada('Primeiro número:\n')
             print()
-            prox_num = obter_entrada('Próximo número:\n')
 
-            # Executa a operação e atualiza o número
-            resultado = executar_operacao(numero, operador, prox_num)
+            while True:
+                operador = input('Operador:\n').lower()
 
-            limpar_tela()
-            print('L = listar operadores e comandos.\n')
-            print(f'Resultado:\n{resultado}\n\n')
+                if processar_comando(operador):
+                    continue
 
-            if operador == '%%':
-                break
+                operadores = ['+', '-', '*', '**', '/', '//', '%', '%%', '&']
 
-            # Atualiza o número para a próxima iteração
-            numero = resultado
+                # Verifica se o operador é válido antes de continuar
+                if operador not in operadores:
+                    limpar_tela()
+                    print('Operador ou comando inválido. Tente novamente.')
+                    print()
+                    continue
 
+                # Solicita o próximo número ou comando
+                print()
+                prox_num = obter_entrada('Próximo número:\n')
+
+                # Executa a operação e atualiza o número
+                resultado = executar_operacao(numero, operador, prox_num)
+
+                limpar_tela()
+                print('Pressione "l" para listar operadores e comandos.\n')
+                print(f'Resultado:\n{resultado}\n\n')
+
+                if operador == '%%':
+                    break
+
+                # Atualiza o número para a próxima iteração
+                numero = resultado
+
+    except KeyboardInterrupt:
+        limpar_tela()
 
 ################################ OPERAÇÃO ######################################
-
 
 def executar_operacao(x, operador, y):
     # Dicionário de operadores e suas funções correspondentes
@@ -66,7 +64,7 @@ def executar_operacao(x, operador, y):
         '/': lambda: x / y,
         '//': lambda: x // y,
         '%': lambda: x % y,
-        '%%': lambda: resultado_div_e(x, y),
+        '%%': lambda: divisao_equilibrada(formatar(x), formatar(y)),
         '&': lambda: radiciacao(x, y)
     }
 
@@ -100,31 +98,24 @@ def formatar(numero):
         return int(numero)
     return numero
 
-
 ############################ OPERADORES CRIADOS ################################
 
-
-def divisao_equilibrada(dividendo, divisor, n1='x', n2=''):
+# Fórmula criada por mim
+def divisao_equilibrada(dividendo, divisor):
     quociente = dividendo // divisor
     resto = dividendo % divisor
     next = quociente + 1
-
+    
+    # guard clause pra retornar uma divisão inteira caso não haja resto
     if resto == 0:
-        return f'{quociente} {n1} {divisor} {n2}'
+        return f'{quociente} x {divisor}'
 
+    # guard clause pra retornar uma divisão normal caso haja números flutuantes
     if isinstance(dividendo, float) or isinstance(divisor, float):
         quociente = dividendo / divisor
-        return f'{quociente} {n1} {divisor} {n2}'
+        return f'{quociente} x {divisor}'
 
-    return f'\n{quociente} {n1} {(divisor
-                                  - resto)} {n2}\n{next} {n1} {resto} {n2}'
-
-
-def resultado_div_e(x, y):
-    return divisao_equilibrada(
-        formatar(x), formatar(y),
-        input('\nNome e/ou separador (ou enter): ') or 'x',
-        input('Nome 2 (ou enter): '))
+    return f'\n{quociente} x {(divisor - resto)}\n{next} x {resto}'
 
 
 # Raiz quadrada, cúbica, etc
@@ -133,41 +124,42 @@ def radiciacao(x, y):
     raiz = x ** potencia
     return raiz
 
-
 ################################# COMANDOS #####################################
-
 
 def processar_comando(comando):
     switch_comando = {
-        'l': lista_comandos,
+        'l': listar_comandos,
         'h': exibir_historico,
-        'r': apagar_historico,
-        'p': encerrar_calculadora,
+        'a': apagar_historico,
+        'r': reiniciar_calculadora,
     }
+
     if comando in switch_comando:
         switch_comando[comando]()
         return True
     return False
 
 
-def lista_comandos():
+def listar_comandos():
     limpar_tela()
     print('Operadores disponíveis:')
-    print(' +  : Adição')
-    print(' -  : Subtração')
-    print(' *  : Multiplicação x')
-    print(' ** : Exponenciação ^')
-    print(' /  : Divisão ÷')
-    print(' // : Divisão inteira ÷')
-    print(' %  : Módulo (resto da divisão) ÷')
-    print(' %% : Divisão equilibrada ÷')
-    print(' &  : Radiciação √ (1º número é o radicando e o próximo é o índice)')
+    print('+  | Adição')
+    print('-  | Subtração')
+    print('*  | Multiplicação x')
+    print('** | Exponenciação ^')
+    print('/  | Divisão ÷')
+    print('// | Divisão inteira ÷')
+    print('%  | Módulo (resto da divisão) ÷')
+    print('%% | Divisão equilibrada ÷')
+    print('&  | Radiciação √ (1º número é o radicando e o próximo é o índice)')
+    print('\nObs: o primeiro número passa a ser o resultado após a 1ª conta,')
+    print('mas você pode usar o comando "r" pra resetar.')
     print('\nComandos disponíveis:')
-    print(' l  : Exibir lista de operadores e comandos disponíveis')
-    print(' h  : Histórico das operações')
-    print(' r  : Resetar histórico')
-    print(' f  : Finalizar operação (após ter enviado o 1º número)')
-    print(' p  : Parar o programa\n\n')
+    print('l  | Lista de operadores e comandos disponíveis')
+    print('h  | Histórico das operações')
+    print('a  | Apagar histórico')
+    print('r  | Reiniciar calculadora (finalizar operação)')
+    print('\nCtrl+c | Close\n\n')
 
 
 # Lista para armazenar o histórico das operações
@@ -195,19 +187,11 @@ def apagar_historico():
         print('O histórico já está vazio.\n')
 
 
-def encerrar_calculadora():
+def reiniciar_calculadora():
     limpar_tela()
-    print('Programa encerrado.\n')
-    sys.exit()
+    main()
 
-
-################################ OUTROS ########################################
-
-
-# Função para limpar a tela, compatível com Windows e Unix-based OS
-def limpar_tela():
-    os.system('cls' if os.name == 'nt' else 'clear')
-
+################################################################################
 
 # Executa a função principal se o script for executado diretamente
 if __name__ == "__main__":
