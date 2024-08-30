6ed182da32a83bb86f59fb8a8ba71b80414d9fcd
britokaiquee <kaique.brito.rob@gmail.com>
2024-08-29 21:50:03 -0300

    Melhorada a validação de operadores e tratamento de erros

--- calculadora.py
+++ calculadora.py
@@ -1,38 +1,42 @@
 while True:
     try:
         entrada = input(
-'\nDigite "I" para iniciar a operação ou "P" para parar, e pressione Enter. \n')
-        if entrada == 'I' or entrada == 'i':
-            print('\nCalculadora v0.2.0\n')
+            '\nDigite "I" para iniciar a operação ou "P" para parar: ')
+        if entrada.lower() == 'i':
+            print('\nCalculadora v0.3.0\n')
             num1 = float(input('Primeiro número:\n'))
-            num2 = float(input('Segundo número:\n'))
             operador = input('Operador:\n')
-            if operador == '+':
-                resultado = num1 + num2
-            elif operador == '-':
-                resultado = num1 - num2
-            elif operador == '*':
-                resultado = num1 * num2
-            elif operador == '/':
-                resultado = num1 / num2
-            elif operador == '//':
-                resultado = num1 // num2
-            elif operador == '**':
-                resultado = num1 ** num2
-            elif operador == '%':
-                resultado = num1 % num2
+            if operador in ['+', '-', '*', '/', '//', '**', '%']:
+                num2 = float(input('Segundo número:\n'))
+                if operador == '+':
+                    resultado = num1 + num2
+                elif operador == '-':
+                    resultado = num1 - num2
+                elif operador == '*':
+                    resultado = num1 * num2
+                elif operador == '/':
+                    resultado = num1 / num2
+                elif operador == '//':
+                    resultado = num1 // num2
+                elif operador == '**':
+                    resultado = num1 ** num2
+                elif operador == '%':
+                    resultado = num1 % num2
+                else:
+                    print('Erro: operador inválido.')
+                    continue
+
+                # Se o resultado é um número inteiro, converta-o para int
+                if resultado.is_integer():
+                    resultado = int(resultado)
+                print(f'\nResultado:\n{resultado}')
             else:
                 print('Erro: operador inválido.')
-                continue
-
-            # Se o resultado é um número inteiro, converta-o para int
-            if resultado.is_integer():
-                resultado = int(resultado)
-            print(f'\nResultado = {resultado}')
-        elif entrada == 'P' or entrada == 'p':
+        elif entrada.lower() == 'p':
             print('\nVocê saiu.')
             break
         else:
             print('Erro: você não digitou nenhuma das opções.')
-    except:
+    except ValueError:
         print('Erro: valor inválido.')
+        
