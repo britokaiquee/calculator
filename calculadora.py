ffe5a5255214160e766f6631b712c118038d4d56
britokaiquee <kaique.brito.rob@gmail.com>
2024-08-29 22:31:26 -0300

    Adicionado resultado parcial e opção de finalizar

--- calculadora.py
+++ calculadora.py
@@ -3,12 +3,15 @@ while True:
         entrada = input(
             '\nDigite "I" para iniciar a operação ou "P" para parar: ')
         if entrada.lower() == 'i':
-            print('\nCalculadora v0.4.0\n')
+            print('\nCalculadora v0.5.0\n')
             resultado = float(input('Primeiro número:\n'))
             while True:
+                if resultado.is_integer():
+                    resultado = int(resultado)
+                print(f'\nResultado total: {resultado}')
                 operador = input(
-                    'Operador (ou "R" para mostrar o resultado):\n')
-                if operador.lower() == 'r':
+                    'Operador (ou "F" para finalizar):\n')
+                if operador.lower() == 'f':
                     break
                 if operador in ['+', '-', '*', '/', '//', '**', '%']:
                     num = float(input('Próximo número:\n'))
@@ -30,11 +33,8 @@ while True:
                     print('Erro: operador inválido.')
                     continue
 
-            if resultado.is_integer():
-                resultado = int(resultado)
-            print(f'\nResultado:\n{resultado}')
         elif entrada.lower() == 'p':
-            print('\nVocê saiu.')
+            print('\nPrograma encerrado.')
             break
         else:
             print('Erro: você não digitou nenhuma das opções.')
